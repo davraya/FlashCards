a5c4130ba47a3f11763903c1d6bc58fd1f6e061a
Kevin <kevin.castro.chavez0@gmail.com>
2022-10-24 21:08:19 -0600

    created decks view and some hard coded decks

--- app/src/main/java/david/raya/flashcards/SplashScreen.kt
+++ app/src/main/java/david/raya/flashcards/SplashScreen.kt
@@ -11,7 +11,7 @@ class SplashScreen : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
         Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, DecksList::class.java))
             finish()
         }, 2500)
     }
