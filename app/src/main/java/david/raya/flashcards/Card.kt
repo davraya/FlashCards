ff2fe6a810914bd82a2405baaeb4cb4bc6f924cb
Efrain Gomez Fajardo <gom21023@byui.edu>
2022-11-07 14:57:38 -0700

    Created Card class

---
+++ app/src/main/java/david/raya/flashcards/Card.kt
@@ -0,0 +1,4 @@
+package david.raya.flashcards
+
+// This is the Card class. This is what will the user will be creating.
+data class Card (var front: String="Lorem Ipsum", var back: String="Dolor Sit Amet")
