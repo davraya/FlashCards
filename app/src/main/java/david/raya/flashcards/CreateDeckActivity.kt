367c61bb355c473993c3a398a6666e2685d530d7
Kevin <kevin.castro.chavez0@gmail.com>
2022-11-28 15:36:54 -0700

    created and made work back button

--- app/src/main/java/david/raya/flashcards/CreateDeckActivity.kt
+++ app/src/main/java/david/raya/flashcards/CreateDeckActivity.kt
@@ -1,11 +1,20 @@
 package david.raya.flashcards
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.widget.Toolbar
 
 class CreateDeckActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_deck)
+
+        var backButton = findViewById<Button>(R.id.back_deck)
+        backButton.setOnClickListener {
+            val backIntent = Intent(this@CreateDeckActivity, DecksList::class.java)
+            startActivity(backIntent)
+        }
     }
 }
