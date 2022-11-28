ee22c08a36555152ad349801bb22865dfa3d3c38
Kevin <kevin.castro.chavez0@gmail.com>
2022-11-28 14:31:04 -0700

    commited code

---
+++ app/src/main/java/david/raya/flashcards/CreateDeckActivity.kt
@@ -0,0 +1,11 @@
+package david.raya.flashcards
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateDeckActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_deck)
+    }
+}
--- app/src/main/java/david/raya/flashcards/DecksList.kt
+++ app/src/main/java/david/raya/flashcards/DecksList.kt
@@ -1,11 +1,18 @@
 package david.raya.flashcards
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class DecksList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_decks_list)
+
+        val fab: View = findViewById(R.id.fab_create_board)
+        fab.setOnClickListener { view ->
+            startActivity(Intent(this@DecksList, CreateDeckActivity::class.java))
+        }
     }
 }
