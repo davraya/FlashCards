fc509a4cadcd72cb06e422373f931f4ba99b7101
Kevin <kevin.castro.chavez0@gmail.com>
2022-11-28 20:27:12 -0700

    added functionality to read data from firebase

--- app/src/main/java/david/raya/flashcards/DecksList.kt
+++ app/src/main/java/david/raya/flashcards/DecksList.kt
@@ -1,9 +1,13 @@
 package david.raya.flashcards
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class DecksList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +18,18 @@ class DecksList : AppCompatActivity() {
         fab.setOnClickListener { view ->
             startActivity(Intent(this@DecksList, CreateDeckActivity::class.java))
         }
+
+        val db = Firebase.firestore
+
+        db.collection("decks")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d(TAG, "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(TAG, "Error getting documents.", exception)
+            }
     }
 }
