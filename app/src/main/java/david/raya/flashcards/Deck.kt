3bae3327499c7d9bc85b973499690009a5688489
Efrain Gomez Fajardo <91625287+EfraOut@users.noreply.github.com>
2022-12-01 14:10:54 -0700

    Update Deck.kt

--- app/src/main/java/david/raya/flashcards/Deck.kt
+++ app/src/main/java/david/raya/flashcards/Deck.kt
@@ -1,11 +1,13 @@
 package david.raya.flashcards
 
+// Data class that will contain a collection of Cards.
 data class Deck(var subject: String="Lorem Ipsum", val card: MutableList<Card>) {
     fun addCard() {
         val aCard = Card()
         card.add(aCard)
     }
-
+    
+    // Edits a selected card by prompting for the content.
     fun edit(card: Card) {
         val promptFront = "New front text"
         val promptBack = "New back text"
