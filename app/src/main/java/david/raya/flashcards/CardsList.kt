cb1cb683ee9359c36340e2004abbf39a26e9b1d5
Kevin <kevin.castro.chavez0@gmail.com>
2022-12-05 14:49:28 -0700

    created the basic card layout

---
+++ app/src/main/java/david/raya/flashcards/CardsList.kt
@@ -0,0 +1,11 @@
+package david.raya.flashcards
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CardsList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cards_list)
+    }
+}
